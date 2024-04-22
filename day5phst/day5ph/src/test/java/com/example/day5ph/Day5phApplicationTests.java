53d419d02467894eda129170d96e466a2a72f9a9
Mithunaa <151151235+Mithunaa106@users.noreply.github.com>
2024-04-22 16:10:02 +0530

    Add files via upload

---
+++ day5phst/day5ph/src/test/java/com/example/day5ph/Day5phApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day5ph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day5phApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
